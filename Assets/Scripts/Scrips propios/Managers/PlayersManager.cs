c486e90eb57237a5e44ee910e4cfd7ec93772a2f
Javieeh <Javierdelaspenas@gmail.com>
2024-05-31 22:23:17 +0200

    Camera host

--- Assets/Scripts/Scrips propios/Managers/PlayersManager.cs
+++ Assets/Scripts/Scrips propios/Managers/PlayersManager.cs
@@ -163,7 +163,7 @@ public class PlayersManager : Singleton<PlayersManager>
     private IEnumerator LoadCircuitScene()
     {
         // Cargar la escena del circuito
-        SceneManager.LoadScene("NascarC");
+        SceneManager.LoadScene("Nascar");
 
         // Esperar a que la escena se cargue
         yield return new WaitForSeconds(1f);
@@ -183,6 +183,7 @@ public class PlayersManager : Singleton<PlayersManager>
                 player.transform.position = startPositions[index].position;
                 player.transform.rotation = startPositions[index].rotation;
                 player.GetComponentInChildren<CarController>().enabled = true;
+                player.GetComponent<PlayerCamera>().enabled = true;   
                 index++;
             }
             else
@@ -219,57 +220,13 @@ public class PlayersManager : Singleton<PlayersManager>
         return startPositions;
     }
 
+    public bool TryGetPlayer(ulong clientId, out GameObject player)
+    {
+        return spawnedPlayers.TryGetValue(clientId, out player);
+    }
+
     private void Update()
     {
         // Opcional: cualquier lógica de actualización
-    }
-}
-
-public class Singleton<T> : NetworkBehaviour where T : Component
-{
-    private static T _instance;
-
-    public static T Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                var objs = FindObjectsOfType(typeof(T)) as T[];
-                if (objs.Length > 0)
-                {
-                    _instance = objs[0];
-                }
-                if (objs.Length > 1)
-                {
-                    Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
-                }
-                if (_instance == null)
-                {
-                    GameObject obj = new GameObject();
-                    obj.name = string.Format("_{0}", typeof(T).Name);
-                    _instance = obj.AddComponent<T>();
-                    DontDestroyOnLoad(obj);
-                }
-                else
-                {
-                    DontDestroyOnLoad(_instance.gameObject);
-                }
-            }
-            return _instance;
-        }
-    }
-
-    protected virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-        }
     }
 }
