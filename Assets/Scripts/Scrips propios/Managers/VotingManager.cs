0922dd40cb176458bbba95ea9782c7253384f309
Javieeh <Javierdelaspenas@gmail.com>
2024-07-03 23:52:31 +0200

    ** voting

--- Assets/Scripts/Scrips propios/Managers/VotingManager.cs
+++ Assets/Scripts/Scrips propios/Managers/VotingManager.cs
@@ -9,60 +9,75 @@ using UnityEngine.UI;
 
 public class VotingManager : Singleton<VotingManager>
 {
-    private NetworkVariable<int> nascarVotes = new NetworkVariable<int>();
-    private NetworkVariable<int> oasisVotes = new NetworkVariable<int>();
-    private NetworkVariable<int> owlPlainsVotes = new NetworkVariable<int>();
-    private NetworkVariable<int> rainyVotes = new NetworkVariable<int>();
+    private NetworkVariable<int> nascarVotes = new NetworkVariable<int>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkVariable<int> oasisVotes = new NetworkVariable<int>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkVariable<int> owlPlainsVotes = new NetworkVariable<int>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkVariable<int> rainyVotes = new NetworkVariable<int>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
+    private Dictionary<ulong, int> playerVotes = new Dictionary<ulong, int>();
 
-    // Start is called before the first frame update
     void Start()
     {
-        //nascarVotes.Value = 0;
-        //oasisVotes.Value = 0;
-        //owlPlainsVotes.Value = 0;
-        //rainyVotes.Value = 0;
+        // Inicialización si es necesario
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Lógica de actualización si es necesario
     }
 
-    
-
-    //PlayerVote
     [ServerRpc(RequireOwnership = false)]
-    public void VoteForMapServerRpc(int mapIndex)
+    public void VoteForMapServerRpc(int mapIndex, ServerRpcParams rpcParams = default)
     {
+        ulong playerId = rpcParams.Receive.SenderClientId;
 
+        if (playerVotes.ContainsKey(playerId))
+        {
+            int lastVote = playerVotes[playerId];
+            DecreaseVote(lastVote);
+        }
+
+        IncreaseVote(mapIndex);
+        playerVotes[playerId] = mapIndex;
+        UpdateVotesClientRpc(mapIndex);
+    }
+
+    private void DecreaseVote(int mapIndex)
+    {
+        switch (mapIndex)
+        {
+            case 0:
+                nascarVotes.Value--;
+                break;
+            case 1:
+                oasisVotes.Value--;
+                break;
+            case 2:
+                owlPlainsVotes.Value--;
+                break;
+            case 3:
+                rainyVotes.Value--;
+                break;
+        }
+    }
+
+    private void IncreaseVote(int mapIndex)
+    {
         switch (mapIndex)
         {
             case 0:
                 nascarVotes.Value++;
                 break;
-
             case 1:
                 oasisVotes.Value++;
-
                 break;
-
             case 2:
                 owlPlainsVotes.Value++;
-
                 break;
-
             case 3:
                 rainyVotes.Value++;
-
-                break;
-
-            default:
                 break;
         }
-
-        UpdateVotesClientRpc(mapIndex); //actualizamos
     }
 
     [ClientRpc]
@@ -74,33 +89,39 @@ public class VotingManager : Singleton<VotingManager>
             case 0:
                 updatedVote = nascarVotes.Value;
                 break;
-
             case 1:
                 updatedVote = oasisVotes.Value;
-
                 break;
-
             case 2:
                 updatedVote = owlPlainsVotes.Value;
-
                 break;
-
             case 3:
                 updatedVote = rainyVotes.Value;
-
-                break;
-
-            default:
                 break;
         }
 
         UIManager.Instance.UpdateMapVotes(updatedVote, mapIndex);
     }
 
-    public int GetWinningMap() //Metodo para elegir el mapa ganador
+    public int GetWinningMap()
     {
-        
-        int winningMap = Math.Max(Math.Max(nascarVotes.Value, oasisVotes.Value), Math.Max(owlPlainsVotes.Value, rainyVotes.Value));
+        Debug.Log(nascarVotes.Value);
+        Debug.Log(oasisVotes.Value);
+        Debug.Log(owlPlainsVotes.Value);
+        Debug.Log(rainyVotes.Value);
+
+        int[] votesArray = { nascarVotes.Value, oasisVotes.Value, owlPlainsVotes.Value, rainyVotes.Value };
+        int max = -1;
+        int winningMap = -1;
+        for (int i = 0; i < votesArray.Length; i++)
+        {
+            if (votesArray[i] > max)
+            {
+                max = votesArray[i];
+                winningMap = i;
+            }
+        }
+        Debug.Log("WINNING MAP: " + winningMap);
         return winningMap;
     }
 }
